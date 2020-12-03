84e65e1f9af4d9dd5ccb4614109840f87faf77a3
belcaid abderrahim <belcaidna@gmail.com>
2020-12-03 10:28:00 +0100

    init

---
+++ src/Customer.java
@@ -0,0 +1,18 @@
+
+public class Customer extends Person {
+	private int badge;
+
+	public Customer(int id, String fristName, String lastName, int telephoneNumber, String email, int badge) {
+		super(id, fristName, lastName, telephoneNumber, email);
+		this.badge = badge;
+	}
+
+	public int getBadge() {
+		return badge;
+	}
+
+	public void setBadge(int badge) {
+		this.badge = badge;
+	}
+
+}
---
+++ src/Pharmacist.java
@@ -0,0 +1,9 @@
+
+public class Pharmacist extends Person {
+
+	public Pharmacist(int id, String fristName, String lastName, int telephoneNumber, String email) {
+		super(id, fristName, lastName, telephoneNumber, email);
+		// TODO Auto-generated constructor stub
+	}
+
+}
